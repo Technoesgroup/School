fe571bc7996384cb72ef35128fbe088d8c11e072
Technoesgroup <technoesgroup@gmail.com>
2025-02-12 18:50:27 +0530

    update downloads pdf

--- Mount-School/src/Component/Committe/CommitteC3.jsx
+++ Mount-School/src/Component/Committe/CommitteC3.jsx
@@ -17,7 +17,7 @@ const DocumentSection = () => {
           "The Parent-Teacher Association (PTA) bridges parents, teachers, and school administration to enhance students' educational experience. It fosters collaboration, supports school initiatives, and organizes events like meetings, workshops, and cultural programs. By engaging in PTA activities, parents contribute to a supportive and inclusive school environment. ",
         image: image1, // Replace with the actual path
         buttonText: "Download",
-        pdf: "/pdfs/CBSE_Holidays.pdf",
+        pdf: "/PDF/PTA.pdf",
       },
       {
         id:"SMC-section",
@@ -26,7 +26,7 @@ const DocumentSection = () => {
           "The SMC ensures efficient school governance, upholding its vision and values. Comprising management, teachers, and parents, it oversees policy decisions, academics, infrastructure, and teacher training. Committed to high-quality education and a safe environment, the SMC drives continuous improvement, fostering holistic development and excellence. ",
         image:image2, // Replace with the actual path
         buttonText: "Download",
-        pdf: "/pdfs/CBSE_Holidays.pdf",
+        pdf: "/PDF/SMC.pdf",
       },
     ];
 
@@ -34,13 +34,13 @@ const DocumentSection = () => {
     
 
 
-   function SectionCard({ id, title, description, image, buttonText, reverse }) {
+   function SectionCard({ id, title, description, image, buttonText, reverse, pdf }) {
     return (
       <div id={id} className={`Committee-section-card ${reverse ? "Committee-reverse" : ""}`}>
         <div className="Committee-text-content">
           <h2>{title}</h2>
           <p>{description}</p>
-          <a href='' download>
+          <a href={pdf} download>
           <button className="Committee-download-btn">{buttonText}</button>
         </a>
         </div>
@@ -62,6 +62,7 @@ const DocumentSection = () => {
         description={sec.description}
         image={sec.image}
         buttonText={sec.buttonText}
+        pdf={sec.pdf} 
       />
     ))}
   </div>
